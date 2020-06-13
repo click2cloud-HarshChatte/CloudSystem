c1217c373bfa6ea1913ed3d5586079f3a6d8a94e
click2cloud-HarshChatte <65886018+click2cloud-HarshChatte@users.noreply.github.com>
2020-06-14 02:38:05 +0530

    Task Update

--- Task/main.go
+++ Task/main.go
@@ -21,7 +21,7 @@ func Reverse(s string) (result string) {
 func isPalindrome(str string) interface{} {
 	if str == Reverse(str) {
 		return true
-	}s
+	}
 	return false
 }
 
@@ -32,10 +32,19 @@ func Palindrome(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Enter a string: ")
 	fmt.Scan(&str)
 	json.NewEncoder(w).Encode(str)
-	if isPalindrome(strings.ToUpper(str)) == true {
-		fmt.Fprint(w, " TRUE.")
+	
+        var reverseString string = ""
+	var length = len(str)
+
+	for i := length - 1; i >= 0; i -- {
+		reverseString = reverseString + string(str[i])
+	}
+
+	
+	if strings.ToLower(str) == strings.ToLower(reverseString) {
+		fmt.Fprintln(w,"TRUE");
 	} else {
-		fmt.Fprint(w, " FALSE.")
+		fmt.Fprintln(w,"FALSE");
 	}
 }
 
